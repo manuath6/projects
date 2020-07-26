0df707b81e7d5f61a75c4737bf2c9e87158036f7
amanuel.dem <amanuel.dem@gmail.com>
2020-07-26 11:50:35 -0400

    Lowered Disk usage threshold for Health_Check script

--- health_checks.py
+++ health_checks.py
@@ -5,7 +5,7 @@ import psutil
 def check_disk_usage(disk):
     du = shutil.disk_usage(disk)
     free = du.free/du.total*100
-    return free > 20
+    return free > 10
 
 def check_cpu_usage():
     usage = psutil.cpu_percent(1)
