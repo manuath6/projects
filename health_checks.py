55f3cbed348e6c3db24ac37ab2a9eeabe8415e44
amanuel.dem <amanuel.dem@gmail.com>
2020-07-26 11:58:40 -0400

    Added a network check function to health_checks.py

--- health_checks.py
+++ health_checks.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 import shutil
 import psutil
+import socket
 
 def check_disk_usage(disk):
     du = shutil.disk_usage(disk)
     free = du.free/du.total*100
     return free > 10
+
+def check_no_network():
+    """Will attempt to resolve Google's URL and return False if it fails"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
 
 def check_cpu_usage():
     usage = psutil.cpu_percent(1)
